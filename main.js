4126779e7f6bc62f4e5d8b513e7edb4d4583ffc9
SoongMingLiang <soongmingliang647@gmail.com>
2022-12-07 15:01:16 +0800

    Declare the DOM elements that available in the html page

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+    const form = document.querySelector("#new_task_form");
+    const new_task = document.querySelector("#new_task_input");
+    const task = document.querySelector("#tasks");
+})
+
+form.addEventListener("submit", (e) =>{
+    
+})
